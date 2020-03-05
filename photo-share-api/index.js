7721533ece58005d979218165f015d256fc8a76f
khanamoto <khanamoto10@gmail.com>
2020-03-05 11:54:52 +0900

    Add type Photo resolver

--- photo-share-api/index.js
+++ photo-share-api/index.js
@@ -3,32 +3,53 @@ const { ApolloServer } = require(`apollo-server`)
 
 // スキーマ（データ要件）
 const typeDefs = `
+    type Photo {
+        id: ID!
+        url: String!
+        name: String!
+        description: String
+    }
+    
     type Query {
         totalPhotos: Int!
+        allPhotos: [Photo!]!
     }
     
     type Mutation {
-        postPhoto(name: String! description: String): Boolean!
+        postPhoto(name: String! description: String): Photo!
     }
 `
 
-// 写真を格納するための配列を定義する
+// 1. ユニークIDをインクリメントするための変数
+var _id = 0
 var photos = []
 
 // リゾルバ（データ取得）
 const resolvers = {
     Query: {
-        // 写真を格納した配列の長さを返す
-        totalPhotos: () => photos.length
+        totalPhotos: () => photos.length,
+        allPhotos: () => photos
     },
 
     // postPhotoミューテーションと対応するリゾルバ
     Mutation: {
         // 引数：親オブジェクト（Mutation）への参照, GraphQL引数（name と description）
         postPhoto(parent, args) {
-            photos.push(args)
-            return true
+            // 2. 新しい写真を作成し、idを生成する
+            var newPhoto = {
+                id: _id++,
+                ...args
+            }
+            photos.push(newPhoto)
+
+            // 3. 新しい写真を返す
+            return newPhoto
         }
+    },
+
+    // ルートに追加されたリゾルバ = トリビアルリゾルバ
+    Photo: {
+        url: parent => `http://yoursite.com/img/${parent.id}.jpg`
     }
 }
 
