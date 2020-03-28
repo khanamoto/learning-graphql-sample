271b93c3fe8ceb98a52b74ea8723485bb3b9bdf8
khanamoto <khanamoto10@gmail.com>
2020-03-28 18:44:35 +0900

    Set up Apollo with React

--- photo-share-client/src/index.js
+++ photo-share-client/src/index.js
@@ -1,22 +1,14 @@
-import ApolloClient, { gql } from 'apollo-boost'
+import React from 'react'
+import { render } from 'react-dom'
+import App from './App'
+import { ApolloProvider } from 'react-apollo'
+import ApolloClient from 'apollo-boost'
 
 const client = new ApolloClient({ uri: 'http://localhost:4000/graphql' })
 
-const query = gql`
-    {
-        totalUsers
-        totalPhotos
-    }
-`
-
-// クライアントを利用してクエリを送信する
-// client.query({ query })
-//     .then(({ data }) => console.log('data', data))
-//     .catch(console.log)
-
-// クエリを送信する前にキャッシュを確認し、
-console.log('cache', client.extract())
-// クエリが解決されたあとでもう一度キャッシュを見てみる
-client.query({ query })
-    .then(() => console.log('cache', client.extract()))
-    .catch(console.log)
+render(
+    <ApolloProvider client = { client }>
+        <App />
+    </ApolloProvider>,
+    document.getElementById('root')
+)
