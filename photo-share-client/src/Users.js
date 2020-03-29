d68026066ef2e9ef966fbf3ebd59b96b8c5dc8a3
khanamoto <khanamoto10@gmail.com>
2020-03-29 15:16:40 +0900

    Add a mutation component

--- photo-share-client/src/Users.js
+++ photo-share-client/src/Users.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Query } from 'react-apollo'
+import { Query, Mutation } from 'react-apollo'
+import { gql } from 'apollo-boost'
 import { ROOT_QUERY } from './App'
 
 const Users = () =>
@@ -15,6 +16,11 @@ const UserList = ({ count, users, refetchUsers }) =>
     <div>
         <p>{count} Users</p>
         <button onClick={() => refetchUsers()}>Refetch Users</button>
+        <Mutation mutation={ADD_FAKE_USERS_MUTAION} variables={{ count: 1 }} refetchQueries={[{ query: ROOT_QUERY }]}>
+            {addFakeUsers =>
+                <button onClick={addFakeUsers}>Add Fake Users</button>
+            }
+        </Mutation>
         <ul>
             {users.map(user =>
                 <UserListItem key={user.githubLogin} name={user.name} avatar={user.avatar} />
@@ -27,5 +33,15 @@ const UserListItem = ({ name, avatar }) =>
         <img src={avatar} width={48} height={48} alt="" />
         {name}
     </li>
+
+const ADD_FAKE_USERS_MUTAION = gql`
+    mutation addFakeUsers($count: Int!) {
+        addFakeUsers(count: $count) {
+            githubLogin
+            name
+            avatar
+        }
+    }
+`
 
 export default Users
