573688cc33718d649744f79ae8b2298f655c3df0
khanamoto <khanamoto10@gmail.com>
2020-03-29 23:30:08 +0900

    Add user identification

--- photo-share-client/src/AuthorizedUser.js
+++ photo-share-client/src/AuthorizedUser.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { withRouter } from 'react-router-dom'
-import { Mutation } from 'react-apollo'
+import { Query, Mutation } from 'react-apollo'
 import { gql } from 'apollo-boost'
 import { ROOT_QUERY } from './App'
 
@@ -10,6 +10,28 @@ const GITHUB_AUTH_MUTATION = gql`
         githubAuth(code: $code) { token }
     }
 `
+
+const Me = ({ logout, requestCode, signingIn }) =>
+    // ROOT_QUERYから現在のユーザーに課するデータを取得
+    <Query query={ROOT_QUERY}>
+        {({ loading, data }) => data.me ?
+            <CurrentUser {...data.me} logout={logout} /> :
+            loading ?
+                <p>loading...</p> :
+                <button
+                    onClick={requestCode}
+                    disabled={signingIn}>
+                    Sign In with GitHub
+                </button>
+        }
+    </Query>
+
+const CurrentUser = ({ name, avatar, logout }) =>
+    <div>
+        <img src={avatar} width={48} height={48} alt="" />
+        <h1>{name}</h1>
+        <button onClick={logout}>logout</button>
+    </div>
 
 class AuthorizedUser extends Component {
     state = { signingIn: false }
@@ -35,11 +57,16 @@ class AuthorizedUser extends Component {
 
     render() {
         return (
-            <Mutation mutation={GITHUB_AUTH_MUTATION} update={this.authorizationComplete} refetchQueries={[{ query: ROOT_QUERY }]}>
+            <Mutation
+                mutation={GITHUB_AUTH_MUTATION}
+                update={this.authorizationComplete}
+                refetchQueries={[{ query: ROOT_QUERY }]}>
                 {mutation => {
                     this.githubAuthMutation = mutation
                     return (
-                        <button onClick={this.requestCode} disabled={this.state.signingIn}>Sign In with GitHub</button>
+                        <Me sigingIn={this.state.signingIn}
+                            requestCode={this.requestCode}
+                            logout={() => localStorage.removeItem('token')} />
                     )
                 }}
             </Mutation>
