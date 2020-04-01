a3242373d8d200298935e838875d6b6a85a0c5a3
khanamoto <khanamoto10@gmail.com>
2020-04-01 22:48:05 +0900

    Update user cache

--- photo-share-client/src/Users.js
+++ photo-share-client/src/Users.js
@@ -15,7 +15,7 @@ const UserList = ({ count, users, refetchUsers }) =>
     <div>
         <p>{count} Users</p>
         <button onClick={() => refetchUsers()}>Refetch Users</button>
-        <Mutation mutation={ADD_FAKE_USERS_MUTAION} variables={{ count: 1 }} refetchQueries={[{ query: ROOT_QUERY }]}>
+        <Mutation mutation={ADD_FAKE_USERS_MUTAION} variables={{ count: 1 }} update={updateUserCache}>
             {addFakeUsers =>
                 <button onClick={addFakeUsers}>Add Fake Users</button>
             }
@@ -26,6 +26,18 @@ const UserList = ({ count, users, refetchUsers }) =>
             )}
         </ul>
     </div>
+
+const updateUserCache = (cache, { data:{ addFakeUsers} }) => {
+    let data = cache.readQuery({ query: ROOT_QUERY })
+    // 総ユーザー数を増やす
+    data.totalUsers += addFakeUsers.length
+    // 現在のユーザーリストとミューテーションから受け取ったフェイクユーザーを結合する
+    data.allUsers = [
+        ...data.allUsers,
+        ...addFakeUsers
+    ]
+    cache.writeQuery({ query: ROOT_QUERY, data })
+}
 
 const UserListItem = ({ name, avatar }) =>
     <li>
